6f298addb12667716887423c2f142ef601df2e52
Ben Bruder <bbruder@mail.yu.edu>
2023-01-23 14:09:42 -0800

    Initial Commit

---
+++ testapp/testappApp.swift
@@ -0,0 +1,17 @@
+//
+//  testappApp.swift
+//  testapp
+//
+//  Created by Ben Bruder on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct testappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
